0c4078e016ea3f2b4d8095bf443aaf9f53c8f81b
Erik Stenman <erik@stenmans.org>
2019-06-24 16:45:04 +0200

    Fix stand alone fate.

--- apps/aefate/src/aefate.erl
+++ apps/aefate/src/aefate.erl
@@ -7,6 +7,9 @@
 -module(aefate).
 -export([main/1]).
 
+-include_lib("aecontract/include/hard_forks.hrl").
+-include_lib("aecore/include/blocks.hrl").
+-include_lib("aeminer/include/aeminer.hrl").
 
 -define(OPT_SPEC,
     [ {obj_file, undefined, undefined, string, "Fate bytecode file"}
@@ -20,6 +23,9 @@
 %%%===================================================================
 
 main(Args) ->
+    io:format("Loaded mods: ~p\n\n", [lists:sort(code:all_loaded())]),
+    io:format("Paths: ~p\n\n", [code:get_path()]),
+
     case getopt:parse(?OPT_SPEC, Args) of
         {ok, {Opts, []}} ->
             case proplists:get_value(help, Opts, false) of
@@ -64,21 +70,56 @@ load_file(FileName, Opts) ->
             {ok, File} = file:read_file(FileName),
             Code = aeb_fate_code:deserialize(File),
             SerializedCall = aeb_fate_asm:function_call(Call),
-            What = #{ contract => FileName
-                    , call => SerializedCall},
-            Chain = #{ contracts =>
-                           #{ FileName => Code}},
-            case aefa_fate:run(What, Chain) of
-                {ok, Env} ->
-                    print_after_run(Verbose, Code, Chain, Env),
+            GasPrice = 1,
+            Origin = <<1:256>>,
+            Caller = <<1:256>>,
+            Trees = aec_trees:new_without_backend(),
+
+            TxEnv = tx_env(),
+
+            What = #{ contract => <<0:256>>
+                    , call => SerializedCall
+                    , gas => 10000000000
+                    , value => 0
+                    , store => <<>>
+                    , code => File},
+            Env = #{ gas_price => GasPrice
+                   , origin => Origin
+                   , caller => Caller
+                   , trees  => Trees
+                   , tx_env => TxEnv
+                   },
+
+            case aefa_fate:run(What, Env) of
+                {ok, NewEnv} ->
+                    print_after_run(Verbose, Code, Env, NewEnv),
                     io:format("~0p~n", [aefa_engine_state:accumulator(Env)]);
-                {error, Error, Env} ->
-                    print_after_run(Verbose, Code, Chain, Env),
+                {error, Error, NewEnv} ->
+                    print_after_run(Verbose, Code, Env, NewEnv),
                     io:format("~p~n", [Error])
             end
     end.
 
 print_after_run(true, Code, Chain, Env) ->
     io:format("Code: ~0p~n", [Code]),
-    io:format("Chain: ~0p~n", [Chain]),
+    %% io:format("Chain: ~0p~n", [Chain]),
     io:format("Env: ~0p~n", [Env]).
+
+tx_env() ->
+    Height = 1,
+    RootHash =  <<0:?BLOCK_HEADER_HASH_BYTES/unit:8>>,
+    PrevHash =  <<1:?BLOCK_HEADER_HASH_BYTES/unit:8>>,
+    PrevKeyHash =  <<2:?BLOCK_HEADER_HASH_BYTES/unit:8>>,
+    KeyHash =  <<3:?BLOCK_HEADER_HASH_BYTES/unit:8>>,
+    Miner = 0,
+    Beneficiary =  aec_block_genesis:beneficiary(),
+    Target = aeminer_pow:integer_to_scientific(1),
+    Evd = 1,
+    Nonce = 1,
+    Time = aeu_time:now_in_msecs(),
+    Version =  ?LIMA_PROTOCOL_VSN,
+
+    aetx_env:tx_env_from_key_header(
+      aec_headers:new_key_header(Height, PrevHash, PrevKeyHash, RootHash, Miner,
+                                 Beneficiary, Target, Evd, Nonce, Time, Version),
+      KeyHash, Time, PrevHash).
